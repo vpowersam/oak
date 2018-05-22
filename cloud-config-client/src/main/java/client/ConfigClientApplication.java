cd9ec59535cafb36eec6e82b54eb67d90f46959d
sam <sam@DESKTOP-DLQ9HIP>
2018-05-22 09:41:53 +0800

    11

--- cloud-config-client/src/main/java/client/ConfigClientApplication.java
+++ cloud-config-client/src/main/java/client/ConfigClientApplication.java
@@ -1,13 +1,27 @@
 package client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RefreshScope
+@RestController
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+	
+	@Value("${message}")
+	private String message;
+	
+	@RequestMapping("/message")
+	public String message() {
+		return message;
 	}
 
 }
